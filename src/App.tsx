935ad75be18af774e123e9f336c7e99df27943fc
0603anton <zorgzb@gmail.com>
2022-09-25 09:02:43 +0200

    useState - task start

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
 import React from 'react';
 import './App.css';
-import {Header} from './site/Header';
-import {Body} from './site/Body';
-import {Footer} from './site/Foooter';
-import {NewComponent} from './site/NewComponent';
 
-const topCars = [
-    {manufacturer: 'BMW', model: 'm5cs'},
-    {manufacturer: 'Mercedes', model: 'e63s'},
-    {manufacturer: 'Audi', model: 'rs6'}
-]
 
 function App() {
     return (
         <>
-            <Header title={'NEW THEADER'}/>
-            <Body titleForBody={'NEW BODY'}/>
-            <Footer titleForFooter={'NEW FOOTER'}/>
-            <NewComponent cars={topCars}/>
+
         </>
     );
 }
