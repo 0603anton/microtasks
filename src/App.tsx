7e6f3efbb0836b16f6e1070a16d30f4a1aef75a3
0603anton <zorgzb@gmail.com>
2022-10-11 21:04:32 +0200

    input in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,87 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-// import {Header} from './site/Header';
-// import {Body} from './site/Body';
-// import {Footer} from './site/Foooter';
-// import {NewComponent} from './site/NewComponent';
-// import {NewComponentTable} from './site/NewComponentTable';
-// import {MouseEvent} from 'react';
-import {Button} from "./components/Button";
 
-
-// const topCars = [
-//     {manufacturer: 'BMW', model: 'm5cs'},
-//     {manufacturer: 'Mercedes', model: 'e63s'},
-//     {manufacturer: 'Audi', model: 'rs6'},
-//     {manufacturer: 'BMW', model: 'm5cs'},
-//     {manufacturer: 'Mercedes', model: 'e63s'},
-//     {manufacturer: 'Audi', model: 'rs6'}
-// ]
+type MessageType = {
+    message: string
+}
 
 function App() {
-    //   const myFirstSubscriber = (event: MouseEvent<HTMLButtonElement>) => {
-    //      console.log(`Hello, my name is Vasya`)
-    //  }
-
-    //   const mySecondSubscriber = (event: MouseEvent<HTMLButtonElement>) => {
-    //    console.log(`Hello, my name is Ivan`)
-    //  }
-
-    // const onClickHandler = (name: string) => {
-    //     return console.log(name)
-    // }
-
-    // const foo1 = () => {
-    //     // ничего не передаём
-    //     console.log(100200)
-    // }
-    // const foo2 = (number: number) => {
-    //     return console.log(number)
-    // }
-
-    const Button1Foo = (subscriber:string,age:number) => {
-        console.log(subscriber, age)
-    }
-
-    const Button2Foo = (subscriber:string, age:number) => {
-        console.log(subscriber, age)
-    }
-    const Button3Foo = () => {
-        console.log("I`m stupid button")
-    }
+    const [message, setMessage] = useState<Array<MessageType>>([
+            {message: 'message1'},
+            {message: 'message2'},
+            {message: 'message3'},
+            {message: 'message4'},
+            {message: 'message5'}
+        ]
+    )
     return (
-        <>
-
-            <Button name={'MyYouTubeChannel-1'} callBack={()=>Button1Foo('I`m Vasya', 21)}/>
-            <Button name={'MyYouTubeChannel-2'} callBack={()=>Button2Foo('I`m Ivan',32 )}/>
-            <Button name={'MyYouTubeChannel-3'} callBack={Button3Foo}/>
-
-
-            {/*<button onClick={(event: MouseEvent<HTMLButtonElement>) => onClickHandler(`Vasya`)}>MyYouTubeChannel-1*/}
-            {/*</button>*/}
-            {/*<button onClick={(event: MouseEvent<HTMLButtonElement>) => onClickHandler(`Ivan`)}>MyYouTubeChannel-2*/}
-            {/*</button>*/}
-
-
-            <>
-                {/*<button onClick={() => {*/}
-                {/*    console.log(`Hello`)*/}
-                {/*}}>MyYouTubeChannel-1*/}
-                {/*</button>*/}
-                {/*<button onClick={(event: MouseEvent<HTMLButtonElement>) => onClickHandler(`Ivan`)}>MyYouTubeChannel-3*/}
-                {/*</button>*/}
-
-                {/*<button onClick={foo1}>1</button>*/}
-                {/*<button onClick={() => foo2(100200)}>2</button>*/}
-
-                {/*<br/>*/}
-                {/*<Header title={'NEW HEADER'}/>*/}
-                {/*<Body titleForBody={'NEW BODY'}/>*/}
-                {/*<Footer titleForFooter={'NEW FOOTER'}/>*/}
-                {/*<NewComponent cars={topCars}/>*/}
-                {/*<NewComponentTable anotherCars={topCars}/>*/}
-            </>
-        </>
+        <div className="App">
+            <div>
+                <input/>
+                <button>+</button>
+            </div>
+            {message.map((el: MessageType, index: number) => {
+                return (
+                    <div key={index}>{el.message}</div>
+                )
+            })}
+        </div>
     );
 }
 
